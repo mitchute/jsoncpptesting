2390557abc96dd9892df765c9cbcf76da263d94b
Matt Mitchell <matt.s.mitchell@okstate.edu>
2017-10-04 15:19:32 -0500

    Write to text file

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,15 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << std::to_string(42) << std::endl;
 
-    ifstream ifs("C:\\Users\\Matt\\CLionProjects\\untitled\\input\\alice.json");
+    // Input file with json data
+    ifstream ifs("D:\\EnergyPlus\\Projects\\jsoncpptesting\\input\\alice.json");
+
+    // Read json data from file
     Json::Reader reader;
     Json::Value obj;
     reader.parse(ifs, obj); // reader can also read strings
+
+    // Write to terminal
     cout << "Book: " << obj["book"].asString() << endl;
     cout << "Year: " << obj["year"].asUInt() << endl;
     const Json::Value& characters = obj["characters"]; // array of characters
@@ -20,6 +25,15 @@ int main() {
         cout << " chapter: " << characters[i]["chapter"].asUInt();
         cout << endl;
     }
+
+    // Output file to write json data
+    std::ofstream ofs;
+    ofs.open("out.txt");
+
+    Json::StyledWriter styledWriter;
+    ofs << styledWriter.write(obj);
+
+    ofs.close();
 
     system("pause");
     return 0;
